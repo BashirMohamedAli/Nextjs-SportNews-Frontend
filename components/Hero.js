6ced9b9d2a69fbe72111a36c97a9690268e13966
Bashir <takursiin253@gmail.com>
2022-03-20 13:43:51 +0300

    frontend

---
+++ components/Hero.js
@@ -0,0 +1,11 @@
+import styles from "@/styles/Hero.module.css"
+
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className={styles.hero}>
+         <h1>Find OUt the Latest Sport News</h1>
+    </div>
+  )
+}
---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import Layout from '@/components/Layout'
+
+export default function about() {
+  return (
+    <div>
+     <Layout title="About Sport News">
+         <h1>About</h1>
+         <p>
+         App to find out Sport News like cricket, football, f1, tennis, badminton, golf
+         </p>
+     </Layout>
+    </div>
+  )
+}
